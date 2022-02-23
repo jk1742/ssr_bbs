ca4aa8bd63800a87b90e6b29325d994c6a86ec47
jk1742 <jk1742@gmail.com>
2022-02-23 20:40:59 +0900

    set framework

--- ssr_bbs/src/index.js
+++ ssr_bbs/src/index.js
@@ -3,20 +3,28 @@ import _ from 'lodash';
 function component() {
   const element = document.createElement('div');
   const button = document.createElement('button');
+  button.innerHTML = 'load';
+
+  const btn2 = document.createElement('button');
+  btn2.innerHTML = 'clear button';
+
   const br = document.createElement('br');
 
-  button.innerHTML = 'Click me and look at the console!';
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.appendChild(br);
   element.appendChild(button);
-
+  element.appendChild(btn2);
+  let que = [];
   // Note that because a network request is involved, some indication
   // of loading would need to be shown in a production-level site/app.
   button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
-    const print = module.default;
-    print();
+    const scrpt = module.default;
+    que.push(scrpt);
   });
-
+  btn2.onclick = e => {
+    que[0]();
+    // console.log('hello btn2');
+  }
    return element;
 }
 
